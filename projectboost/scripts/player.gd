c5effeebe8eeac3fb5ff09357041bca31732cf9d
ardkoornstra <apckoornstra@gmail.com>
2025-01-26 21:04:43 +0100

    Better player controls

--- projectboost/scripts/player.gd
+++ projectboost/scripts/player.gd
@@ -1,16 +1,14 @@
-extends Node3D
-
+extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("Boost"):
+		apply_central_force(basis.y * delta * 1000.0)
+	if Input.is_action_pressed("Left"):
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+	if Input.is_action_pressed("Right"):
+		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
