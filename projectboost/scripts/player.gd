179188f74a05dd2862f20dea391b66810099f9f2
ardkoornstra <apckoornstra@gmail.com>
2025-01-26 21:35:22 +0100

    Collision detection

--- projectboost/scripts/player.gd
+++ projectboost/scripts/player.gd
@@ -1,14 +1,19 @@
 extends RigidBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+@export var thrust: float = 1000.0
+@export var torqueThrust: float = 100.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Boost"):
 		apply_central_force(basis.y * delta * 1000.0)
 	if Input.is_action_pressed("Left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torqueThrust * delta))
 	if Input.is_action_pressed("Right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torqueThrust * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Goal"):
+		print("OwO")
+	elif body.is_in_group("Hazard"):
+		print("aaaaaaaaAAAAAAAAAAAAAAAAA")
