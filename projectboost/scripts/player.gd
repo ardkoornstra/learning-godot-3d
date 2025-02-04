7ace4f3685408986f7b861791abb469aa8b835ee
ardkoornstra <apckoornstra@gmail.com>
2025-02-04 23:24:32 +0100

    Particle effects

--- projectboost/scripts/player.gd
+++ projectboost/scripts/player.gd
@@ -8,18 +8,31 @@ var is_transitioning: bool = false
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
 @onready var win_audio: AudioStreamPlayer = $WinAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var booster_particles_left: GPUParticles3D = $BoosterParticlesLeft
+@onready var booster_particles_right: GPUParticles3D = $BoosterParticlesRight
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Boost"):
 		apply_central_force(basis.y * delta * 1000.0)
+		booster_particles.emitting = true
 		if !rocket_audio.playing:
 			rocket_audio.play()
 	else:
+		booster_particles.emitting = false
 		rocket_audio.stop()
 	if Input.is_action_pressed("Left"):
 		apply_torque(Vector3(0.0, 0.0, torqueThrust * delta))
+		booster_particles_right.emitting = true
+	else:
+		booster_particles_right.emitting = false
 	if Input.is_action_pressed("Right"):
 		apply_torque(Vector3(0.0, 0.0, -torqueThrust * delta))
+		booster_particles_left.emitting = true
+	else:
+		booster_particles_left.emitting = false
 
 
 func _on_body_entered(body: Node) -> void:
@@ -30,21 +43,27 @@ func _on_body_entered(body: Node) -> void:
 			crash()
 
 func crash() -> void:
+	reset()
 	print("aaaaAAAAAAAAA")
-	rocket_audio.stop()
+	explosion_particles.emitting = true
 	explosion_audio.play()
-	set_process(false)
-	is_transitioning = true
 	var tween = create_tween()
 	tween.tween_interval(explosion_audio.stream.get_length())
 	tween.tween_callback(get_tree().reload_current_scene)
 
 func win(nextLevel: String) -> void:
+	reset()
 	print("OwO")
-	rocket_audio.stop()
+	success_particles.emitting = true
 	win_audio.play()
-	set_process(false)
-	is_transitioning = true
 	var tween = create_tween()
 	tween.tween_interval(win_audio.stream.get_length())
 	tween.tween_callback(get_tree().change_scene_to_file.bind(nextLevel))
+	
+func reset() -> void:
+	set_process(false)
+	rocket_audio.stop()
+	booster_particles.emitting = false
+	booster_particles_left.emitting = false
+	booster_particles_right.emitting = false
+	is_transitioning = true
